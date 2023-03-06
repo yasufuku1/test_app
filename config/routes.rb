21d0027bb0df889efaf01957745fc3b31fd15a4f
yasusu_ 500 <eriyasu@gmail.com>
2023-03-06 14:19:34 +0900

    [Update]user_show追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   }
   resources :tests
   resources :tasks
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 end
