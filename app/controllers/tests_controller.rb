a41bc40a9ee41a5bdec6ebbf60da505ef008d737
yasusu_ 500 <eriyasu@gmail.com>
2023-03-07 18:01:22 +0900

    [Update]tests/new.html.erb,create.html.erb作成

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,6 +2,25 @@ class TestsController < ApplicationController
     before_action :authenticate_user!
 
     def index
-        
+        @tests = Test.all
+    end
+
+    def new
+        @test = Test.new
+    end
+
+    def create
+        @test = current_user.tests.new(test_params)
+        if @test.save
+            redirect_to tests_path, notice: "テストを作成しました"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def test_params
+        params.require(:test).permit(:title)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_03_06_020601) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.integer "task_id"
+    t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
